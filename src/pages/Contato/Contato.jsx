9305507b26cee846f819d63bf49677a16cdf96a5
nameless <130059555+MeErickss@users.noreply.github.com>
2024-09-04 10:56:16 -0300

    pastas

---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,8 @@
+import { styles } from "./Contato.module.css"
+
+
+export default function Contato(){
+    return(
+        <div className={styles.Contato}></div>    
+    )
+}
---
+++ src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,8 @@
+import { styles } from "./Sobre.module.css" 
+
+
+export default function Sobre(){
+    return(
+        <div className={styles.Sobre}></div>
+    )
+}
